a812e4305ad3856a0fc96cdbee8c83f8d1b130e6
Mohamed Saber <mohamed.saber.7753@gmail.com>
2024-11-26 23:42:07 +0200

    start learning php example 1 update 1

--- handleOp.php
+++ handleOp.php
@@ -32,5 +32,23 @@ if (isset(
     }
 } else {
     echo "Please fill in all fields";
+    // Display the form if it hasn't been submitted yet
+    echo "<form action='" . $_SERVER['PHP_SELF'] . "' method='get'>
+    <label for='num1'>Number 1:</label>
+    <input type='number' id='num1' name='num1'><br><br>
+    <label for='num2'>Number 2:</label>
+    <input type='number' id='num2' name='num2'><br><br>
+    <label for='operation'>Operation:</label>
+    <select id='operation' name='operation'>
+    <option value='add'>Add</option>
+    \
+    <option value='subtract'>Subtract</option>
+    \
+    <option value='multiply'>Multiply</option>
+    \
+    <option value='divide'>Divide</option>
+    </select><br><br>
+    <input type='submit' name='submit' value='Calculate'/>
+    </form>";
 }
 ?>
